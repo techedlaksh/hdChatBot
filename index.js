f9d92a80596f69cd91f4f519622b9a45b7982811
techedlaksh <laksharora28@gmail.com>
2017-01-05 06:05:50 +0530

    updated the bot to speak

--- index.js
+++ index.js
@@ -45,3 +45,22 @@ app.post('/webhook/', function (req, res) {
 })
 
 const token = process.env.hdChatBot_fb_page_access_token
+
+function sendTextMessage(sender, text) {
+    let messageData = { text:text }
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token:token},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    })
+}
